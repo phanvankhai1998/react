aa198c545753587cbeff0673660c17b9239f7bc6
Khai Phan <phan_van_khai123@yahoo.com>
2021-08-26 21:04:56 +0700

    React Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const element = <h1>Hello world!</h1>
+import Counter from './components/counter';
+
+
+const element = <h1>Hello world!<Counter /></h1>;
 
 ReactDom.render(element, document.getElementById('root'));
